c5991dc30ffc06cf491d097eee25ff4419bb4d47
fatima thawerani <160662143+fatimathawerani@users.noreply.github.com>
2025-11-02 01:40:06 +0500

    Add files via upload

---
+++ day-02/main.js
@@ -0,0 +1,7 @@
+// console.log(`John elia once said, "Friends who slay together, stay together"`);
+//let famous_person: string = "John Elia";;
+//let quote: string = 'Friends who slay together, stay together';
+//console.log(`${famous_person} once said, "${quote}"`);  
+var myName = "\t\n Fatima \t\n";
+console.log(myName);
+console.log(myName.trim());
---
+++ day-02/main.ts
@@ -0,0 +1,10 @@
+// console.log(`John elia once said, "Friends who slay together, stay together"`);
+
+//let famous_person: string = "John Elia";;
+//let quote: string = 'Friends who slay together, stay together';
+//console.log(`${famous_person} once said, "${quote}"`);  
+
+let myName: string = "\t\n Fatima \t\n"; 
+
+console.log(myName); 
+console.log(myName.trim()); 
