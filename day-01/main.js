c7a71a54679f3f8526f294c55c49d2447cd09abf
fatima thawerani <160662143+fatimathawerani@users.noreply.github.com>
2025-11-02 00:58:51 +0500

    Add files via upload

---
+++ day-01/main.js
@@ -0,0 +1,5 @@
+var myName = "Fatima";
+console.log("Hello ".concat(myName, ", Would you like to learn some Typecript today?"));
+console.log(myName.toLowerCase());
+console.log(myName.toUpperCase());
+console.log(myName.charAt(0).toUpperCase() + myName.slice(1).toLowerCase());
---
+++ day-01/main.ts
@@ -0,0 +1,8 @@
+let myName: string= "Fatima";
+
+console.log(`Hello ${myName}, Would you like to learn some Typecript today?`);
+
+
+console.log (myName.toLowerCase());
+console.log (myName.toUpperCase());
+console.log (myName.charAt(0).toUpperCase() + myName.slice(1).toLowerCase());
